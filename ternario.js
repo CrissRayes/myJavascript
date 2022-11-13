3ba45be0ba13e677a6af51d107aeaa20569214d9
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-11-13 17:28:17 -0300

    ADD: Agrega archivo
    Agrega el archivo ternario.js

---
+++ ternario.js
@@ -0,0 +1,11 @@
+// example ternary operator
+function findGreater(a, b) {
+  return a > b ? "a is greater" : "b is greater"
+}
+
+// myltiple ternary operator
+function findGreaterOrEqual(a, b) {
+  return a === b ? "a and b are equal"
+    : a > b ? "a is greater"
+      : "b is greater"
+}
