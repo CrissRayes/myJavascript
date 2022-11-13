b79ede5480b7c263fb8c4598c44b1dd869d26b3d
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-11-13 17:10:00 -0300

    CGH: Reordena el contenido del archivo
    Se deja primero los ejemplos de Math.random y luego los del modulo crypto de node

--- random_number_in_range.js
+++ random_number_in_range.js
@@ -1,9 +1,22 @@
-// generate random number between 1 and 10 with Math.random()
+// generate random number between 1 and 20 with Math.random()
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(random(1, 10)); // 1 - 10
+console.log(random(1, 20)); // 1 - 20
 
+// step by step how to use Math.random()
+console.log(Math.random()); // 0.0392156862745098
+console.log(Math.floor(Math.random() * (10 - 1 + 1) + 1)); // 1
+
+// generate random number with Math.random()
+const random = () => {
+  return Math.floor(Math.random() * 10) + 1;
+}
+console.log(random()); // 1 - 10
+
+// Using node crypto module to generate random number
+// how to require crypto module
+const crypto = require('crypto');
 // how to use crypto.randomBytes() to generate a random number
 const random = () => {
   return crypto.randomBytes(1).readUInt8(0);
@@ -18,7 +31,7 @@ const random = (min, max) => {
 }
 console.log(random(1, 10)); // 1 - 10
 
-// how to use crypto.randomBytes
+// example step by step how to use crypto.randomBytes
 const crypto = require('crypto');
 const buffer = crypto.randomBytes(1);
 console.log(buffer); // <Buffer 0a>
@@ -26,12 +39,3 @@ console.log(buffer.readUInt8(0)); // 10
 console.log(buffer.readUInt8(0) / (0xff + 1)); // 0.0392156862745098
 console.log(Math.floor(0.0392156862745098 * (10 - 1 + 1) + 1)); // 1
 
-// how to use Math.random()
-console.log(Math.random()); // 0.0392156862745098
-console.log(Math.floor(Math.random() * (10 - 1 + 1) + 1)); // 1
-
-// como usar Math.random() para generar un numero aleatorio entre 1 y 10
-const random = () => {
-  return Math.floor(Math.random() * (10 - 1 + 1) + 1);
-}
-console.log(random()); // 1 - 10
