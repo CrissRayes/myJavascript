2eedb9fed4c7ffaaeca3fbccc1503da593d80cd6
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-11-22 22:44:33 -0300

    reordena directorios

---
+++ calculoTotal/assets/js/app.js
@@ -0,0 +1,16 @@
+const precio = Number(document.querySelector('#precio').innerHTML)
+const unidades = document.querySelector('#unidades')
+const resultado = document.querySelector('#resultado')
+const boton = document.querySelector('#boton')
+const color = document.querySelector('#color')
+const circulo = document.querySelector('#circulo')
+
+color.onchange = (e) => {
+  e.preventDefault()
+  circulo.style.backgroundColor = color.value
+}
+
+boton.onclick = (e) => {
+  e.preventDefault()
+  resultado.innerHTML = precio * unidades.value
+}
