5af4cda6e8696f5f39fac92d7f605e5d78b2c0da
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-01 19:55:15 -0300

    agrega dir condiciones y modifica file promises

---
+++ condiciones.js
@@ -0,0 +1,13 @@
+let number = 10;
+
+
+if (number < 576) {
+  console.log('xs')
+} else if (number <= 768) {
+  console.log('sm')
+} else if (numbernumber <= 962) {
+  console.log('md')
+} else {
+  console.log('lg')
+}
+
--- promise.js
+++ promise.js
@@ -4,3 +4,13 @@ const promise = new Promise((resolve, reject) => {
     resolve('Hola Mundo');
   }, 3000);
 });
+
+// await async
+// Path: async-await.js
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'))
+  });
+}
