c840393239ed416c6ab635acaba17872e942f7c7
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-14 17:32:11 -0300

    agrega archivo

--- arreglos_y_objetos.js
+++ arreglos_y_objetos.js
@@ -1,22 +1,33 @@
 // ARRAYS
 // Declarar un array
+// const nombres = ['Juan', 'Pedro', 'Elena', 'Maria']
+// // recorrer el array y mostrar los elementos en consola
+// nombres.forEach(nombre => {
+//   console.log(nombre) // Juan, Pedro, Elena, Maria
+// })
+
 const nombres = ['Juan', 'Pedro', 'Elena', 'Maria']
-// recorrer el array y mostrar los elementos en consola
-nombres.forEach(nombre => {
+// recorrer el array con un for of
+for (const nombre of nombres) {
   console.log(nombre)
-})
+}
+
+// es posible recorrer el array con un map?
+// map() crea un nuevo arreglo con los resultados de la llamada a la funcion indicada aplicados a cada uno de sus elementos
+const nombresMayusculas = nombres.map(nombre => nombre.toUpperCase())
+console.log(nombresMayusculas) // ['JUAN', 'PEDRO', 'ELENA', 'MARIA']
 
 // agrega elementos a una lista html a partir del arreglo nombres
 const lista = document.querySelector('#lista')
 nombres.forEach(nombre => {
   const item = document.createElement('li')
-  item.textContent = nombre
-  lista.appendChild(item)
+  item.textContent = nombre // le agrega texto al elemento item con el nombre del elemento del arreglo nombres
+  lista.appendChild(item) // appendChild agrega un elemento como hijo de otro elemento
 })
 
 // como obtener ultimo elemento de un array
 const numeros = [1, 2, 3, 4, 5]
-const ultimo = numeros[numeros.length - 1]
+const ultimo = numeros.pop() // pop() elimina el ultimo elemento del arreglo y lo retorna
 console.log(ultimo)
 
 // OBJETOS
@@ -63,3 +74,39 @@ boton2.addEventListener('click', () => {
   lista3.appendChild(item)
 })
 
+const persona = {
+  nombre: 'Juan',
+  apellido: 'Perez',
+  profesion: 'Desarrollador Web',
+  hobby: 'Programar'
+}
+
+console.log(persona.nombre) // Juan
+console.log(persona['apellido']) // Perez
+
+
+
+
+
+// obtener las llaves de un objeto
+const llaves = Object.keys(persona)
+console.log(llaves) // ['nombre', 'apellido', 'profesion', 'hobby']
+// obtener los valores de un objeto
+const valores = Object.values(persona)
+console.log(valores) // ['Juan', 'Perez', 'Desarrollador Web', 'Programar']
+
+// recorrer un objeto con un for in
+for (const key in persona) {
+  console.log(key) // nombre, apellido, profesion, hobby
+  console.log(persona[key]) // Juan, Perez, Desarrollador Web, Programar
+}
+
+// recorrer un objeto con un forEach
+Object.keys(persona).forEach(key => {
+  console.log(key) // nombre, apellido, profesion, hobby
+  console.log(persona[key]) // Juan, Perez, Desarrollador Web, Programar
+})
+
+// recorrer un objeto con un map
+const valores2 = Object.keys(persona).map(key => persona[key])
+console.log(valores2) // ['Juan', 'Perez', 'Desarrollador Web', 'Programar']
