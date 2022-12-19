8d7cd311b84928fcc9422be04e72c93d1965b4d0
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-19 19:00:44 -0300

    agrega cambios y archivos nuevos

--- metodos_de_arreglos.js
+++ metodos_de_arreglos.js
@@ -40,3 +40,37 @@ console.log( arr ) // ['a', 'b', 'c', 'd']
 // splice arguments: the index where the copy starts, the number of elements to remove
 arr.splice( 1, 2 ) // removes the second and third element of the array
 console.log( arr ) // ['a', 'd']
+
+// NEW AT
+// similar to bracket notation but it returns undefined if the index doesn't exist
+const myArr = ['queen', 'beatles', 'rolling stones', 'pink floyd']
+console.log( 'options for getting the third element of an array' )
+console.log( myArr.at( 2 ) ) // 'rolling stones'
+console.log( myArr[2] ) // 'rolling stones'
+// options for getting the last element of an array
+console.log( 'options for getting the last element of an array' )
+console.log( myArr[myArr.length - 1] ) // 'pink floyd'
+console.log( myArr.at( myArr.length - 1 ) ) // 'pink floyd'
+console.log( myArr.slice( -1 ) ) // ['pink floyd']
+console.log( myArr.slice( -1 )[0] ) // 'pink floyd'
+
+// FOREACH
+// forEach method is used to iterate over an array
+
+const movimientos = [200, 450, -400, 3000, -650, -130, 70, 1300]
+movimientos.forEach( function ( movimiento ) {
+  if ( movimiento > 0 ) {
+    console.log( `Depositaste ${movimiento}` )
+  } else {
+    console.log( `Retiraste ${Math.abs( movimiento )}` )
+  }
+} )
+// Even though we can pass just one argument to the callback function, we can pass more than one argument
+// The arguments in a forEach method are:
+// 1.- the current element, example: movimiento
+// 2.- the index of the current element, example: i
+// 3.- the array itself, example: movimientos
+
+movimientos.forEach( function ( movimiento, i, arr ) {
+  console.log( `Tenemos el movimiento ${movimiento} en el índice ${i} | El arreglo completo es: [${arr}]` )
+} )
