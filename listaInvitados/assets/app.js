41e7d818fa699b1ca1eb7eefe700dbd60d10b207
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-19 23:05:10 -0300

    agrega contenido a archivo y crea directorio

--- listaInvitados/assets/app.js
+++ listaInvitados/assets/app.js
@@ -1,21 +1,27 @@
 // Seleccionar elementos del DOM
-const listaDeInvitados = document.querySelector('#invitados')
-const invitadoInput = document.querySelector('#nuevoInvitado')
-const btnAgregar = document.querySelector('#agregarInvitado')
+const listaDeInvitados = document.querySelector( '#invitados' )
+const invitadoInput = document.querySelector( '#nuevoInvitado' )
+const btnAgregar = document.querySelector( '#agregarInvitado' )
 
 // Crear un array para guardar los invitados
 const invitados = []
 // Escuchar el evento click del boton
-btnAgregar.addEventListener('click', () => {
+btnAgregar.addEventListener( 'click', () => {
   const nuevoInvitado = invitadoInput.value // se hace de esta forma porque cada vez que se presiona el boton, lee el valor del input
-  invitados.push(nuevoInvitado)
+
+  if ( nuevoInvitado === '' ) {
+    alert( 'El campo no puede estar vacio' )
+    return // se usa return para que no se siga ejecutando el codigo
+  }
+
+  invitados.push( nuevoInvitado )
   invitadoInput.value = '' // se limpia el input
 
   // iterar sobre el array de invitados y se agrega al DOM
-  let html = ''
-  for (const invitado of invitados) {
+  let html = '' // cada vez que se presiona el boton, se limpia el html para luego recorre el array y agregar los nuevos invitados
+  for ( const invitado of invitados ) {
     html += `<li>${invitado}</li>`
   }
   listaDeInvitados.innerHTML = html
-})
+} )
 
--- metodos_de_arreglos.js
+++ metodos_de_arreglos.js
@@ -1,31 +1,21 @@
-// PUSH
+// PUSH //////////////////////////
 // push add elements to the end of an array
 const a = [1, 2, 3, 4, 5, 6]
 a.push( 7 );
 console.log( a ); // [1, 2, 3, 4, 5, 6, 7]
 
-// UNSHIFT
+// UNSHIFT //////////////////////////
 // unshift add elements to the beginning of an array
 const b = [1, 2, 3, 4, 5, 6]
 b.unshift( 'hola' )
 console.log( b ); // ['hola', 1, 2, 3, 4, 5, 6]
 
-// Ejercicio super héroes
-const superHeroes = [
-  'Ironman',
-  'Spiderman',
-  'Hulk'
-]
-// 1.- Agregar super héroe al final del arreglo
-superHeroes.push( 'Thor' )
-// 2.- Agregar super héroe al principio del arreglo
-superHeroes.unshift( 'Capitan America' )
-
-// el spread operator crea una copia del arreglo original
+// NOTA: el spread operator crea una copia del arreglo original
+const superHeroes = ['Capitan America', 'Ironman', 'Spiderman', 'Hulk']
 const superHeroesCopy = [...superHeroes]
-console.log( superHeroesCopy ) // ['Capitan America', 'Ironman', 'Spiderman', 'Hulk', 'Thor']
+console.log( superHeroesCopy ) // ['Capitan America', 'Ironman', 'Spiderman', 'Hulk']
 
-// SLICE
+// SLICE //////////////////////////
 // slice method is used to copy a portion of an array into a new array
 // takes two arguments: the index where the copy starts and the index where the copy ends and it doesn't include both indexes in the argument
 // example:
@@ -33,17 +23,33 @@ const arr = ['a', 'b', 'c', 'd', 'e']
 console.log( arr.slice( 1, 3 ) ) // ['b', 'c']
 console.log( arr.slice() ) // ['a', 'b', 'c', 'd', 'e'] returns a copy of the original array
 
-// SPLICE
+// SPLICE //////////////////////////
 // splice method is used to remove elements from an array
 // receives two arguments: the index where the removal starts and the number of elements to remove
 // 1.- the index where the removal starts, which is included in the removal
-// 2.- the number of elements to remove, which is included in the removal
+// 2.- the number of elements to be removed
 arr.splice( -1 ) // removes the last element of the array
 console.log( arr ) // ['a', 'b', 'c', 'd']
 arr.splice( 1, 2 ) // removes the second and third element of the array
 console.log( arr ) // ['a', 'd']
 
-// SLICE
+// NOTA: adding elements with splice?
+// splice method is also used to add elements to an array
+// example:
+const arr2 = ['a', 'b', 'c', 'd', 'e']
+arr2.splice( 2, 0, 'x', 'y' ) // adds 'x' and 'y' at the index 2
+console.log( arr2 ) // ['a', 'b', 'x', 'y', 'c', 'd', 'e']
+
+// NOTA: FIND INDEX //////////////////////////
+// findIndex method is used to find the index of an element in an array
+// receives a callback function as an argument
+// example:
+const cocteles = ['Piña Colada', 'Margarita', 'Mojito', 'Caipirinha']
+const index = cocteles.findIndex( ( elemento ) => elemento == "Piña Colada" )
+console.log( index ) // 0
+
+
+// SLICE //////////////////////////
 // slice method is used to copy a portion of an array into a new array
 // takes two arguments:
 // 1.- the index where the copy starts, which is included in the new array
@@ -53,7 +59,7 @@ const newArr = ['a', 'b', 'c', 'd', 'e']
 console.log( newArr.slice( 1, 3 ) ) // ['b', 'c']
 
 
-// AT
+// AT //////////////////////////
 // similar to bracket notation but it returns undefined if the index doesn't exist
 const myArr = ['queen', 'beatles', 'rolling stones', 'pink floyd']
 console.log( 'options for getting the third element of an array' )
@@ -66,7 +72,7 @@ console.log( myArr.at( myArr.length - 1 ) ) // 'pink floyd'
 console.log( myArr.slice( -1 ) ) // ['pink floyd']
 console.log( myArr.slice( -1 )[0] ) // 'pink floyd'
 
-// FOREACH
+// FOREACH //////////////////////////
 // forEach method is used to iterate over an array
 
 const movimientos = [200, 450, -400, 3000, -650, -130, 70, 1300]
