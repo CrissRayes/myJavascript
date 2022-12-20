11ff4f94fa55ae49c4e8ff2685b7c57112e6cb89
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-20 18:40:54 -0300

    funcionalidad de crear y eliminar tareas ok

---
+++ desTodoList/assets/js/app.js
@@ -0,0 +1,68 @@
+// Seleccionar elementos del DOM
+const task = document.querySelector( '#task' )
+const totalTasks = document.querySelector( '#total-tasks' )
+const completedTasks = document.querySelector( '#completed-tasks' )
+const btnAddTask = document.querySelector( '#btn-add-task' )
+const tasksDetail = document.querySelector( '#tasks-detail' )
+
+// crear un array vacio de tareas
+let tasks = []
+
+// Funcion para agregar una tarea
+btnAddTask.addEventListener( 'click', () => {
+  // validar si el input esta vacio
+  if ( task.value === '' ) {
+    alert( 'El campo no puede estar vacío' )
+    return
+  }
+
+  // crear un objeto con la tarea
+  const newTask = {
+    id: Date.now(),
+    name: task.value,
+    completed: false
+  }
+
+  // agregar la tarea al array de tareas
+  tasks.push( newTask )
+
+  // limpiar el input
+  task.value = ''
+
+  // ejecutar la funcion para mostrar las tareas
+  showTasks()
+
+} )
+
+// Funcion para mostrar las tareas
+const showTasks = () => {
+  let tasksList = ''
+  // agregar una tarea por cada elemento del array
+  tasks.forEach( task => {
+    tasksList += `
+    <tr>
+      <td>${task.id}</td>
+      <td>${task.name}</td>
+      <td><input type="checkbox" ${task.completed ? 'checked' : ''}></td>
+      <td><button class="btn btn-danger" onclick="deleteTask(${task.id})">X</button></td>
+    </tr>
+    `
+  } )
+  // agregar el elemento al html
+  tasksDetail.innerHTML = tasksList
+}
+
+// Funcion para eliminar una tarea
+const deleteTask = id => {
+
+  // confirmacion de eliminacion
+  const confirm = window.confirm( '¿Estás seguro de eliminar esta tarea?' )
+  if ( !confirm ) return // si el usuario cancela la confirmacion, terminar la ejecucion de la funcion
+  // filtrar el array de tareas por su id
+  tasks = tasks.filter( task => task.id !== id )
+
+  // alerta de tarea eliminada
+  alert( 'Tarea eliminada' )
+  // ejecutar la funcion para mostrar las tareas
+  showTasks()
+}
