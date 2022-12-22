38612ca19eb07ac9b1939c3128f1b2aa314df651
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-22 14:07:37 -0300

    tarchar tareas completadas

--- desTodoList/assets/js/app.js
+++ desTodoList/assets/js/app.js
@@ -39,8 +39,8 @@ const showTasks = () => {
   tasks.forEach( task => {
     tasksList += `
     <tr>
-      <td>${task.id}</td>
-      <td>${task.name}</td>
+      <td class="${task.completed ? 'completed' : ''}">${task.id}</td>
+      <td class="${task.completed ? 'completed' : ''}">${task.name}</td>
       <td><input type="checkbox" ${task.completed ? 'checked' : ''} onclick="completeTask(${task.id})"></td>
       <td><button class="btn btn-danger" onclick="deleteTask(${task.id})">X</button></td>
     </tr>
