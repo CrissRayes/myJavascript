0c4fa450b31b049777f41901c58490f3acde392f
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-12-26 07:45:05 -0300

    agrega tipos_funciones.js

---
+++ funciones/tipos_funciones.js
@@ -0,0 +1,14 @@
+// function declaration
+function sumar ( a, b ) {
+  return a + b;
+}
+
+// function expression
+const sumar2 = function ( a, b ) {
+  return a + b;
+}
+
+// arrow function
+const sumar3 = ( a, b ) => {
+  return a + b;
+}
